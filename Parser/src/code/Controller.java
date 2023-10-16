b41192014a3dad80a93fe4378cd74efd5ab8b8a8
Antony <tonyxii@gmail.com>
2023-10-16 14:51:00 +0200

    added some lists

--- Parser/src/code/Controller.java
+++ Parser/src/code/Controller.java
@@ -1,22 +1,18 @@
 package code;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
-import javafx.animation.PauseTransition;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class Controller {
 	
@@ -48,6 +44,77 @@ public class Controller {
 	Parser parser = new Parser();
 	
 	private volatile boolean continueRunning = true;
+	
+	List<String> firstNames = List.of(
+			"Wade", "Dave", "Seth", "Ivan", "Riley", "Gilbert", "Jorge", "Dan", "Brian", "Roberto",
+		    "Ramon", "Miles", "Liam", "Nathaniel", "Ethan", "Lewis", "Milton", "Claude", "Joshua", "Glen",
+		    "Harvey", "Blake", "Antonio", "Connor", "Julian", "Aidan", "Harold", "Conner", "Peter", "Hunter",
+		    "Eli", "Alberto", "Carlos", "Shane", "Aaron", "Marlin", "Paul", "Ricardo", "Hector", "Alexis",
+		    "Adrian", "Kingston", "Douglas", "Gerald", "Joey", "Johnny", "Charlie", "Scott", "Martin", "Tristin",
+		    "Troy", "Tommy", "Rick", "Victor", "Jessie", "Neil", "Ted", "Nick", "Wiley", "Morris", "Clark",
+		    "Stuart", "Orlando", "Keith", "Marion", "Marshall", "Noel", "Everett", "Romeo", "Sebastian", "Stefan",
+		    "Robin", "Clarence", "Sandy", "Ernest", "Samuel", "Benjamin", "Luka", "Fred", "Albert", "Greyson",
+		    "Terry", "Cedric", "Joe", "Paul", "George", "Bruce", "Christopher", "Mark", "Ron", "Craig", "Philip",
+		    "Jimmy", "Arthur", "Jaime", "Perry", "Harold", "Jerry", "Shawn", "Walter",
+		    "Daisy", "Deborah", "Isabel", "Stella", "Debra", "Beverly", "Vera", "Angela", "Lucy", "Lauren",
+		    "Janet", "Loretta", "Tracey", "Beatrice", "Sabrina", "Melody", "Chrysta", "Christina", "Vicki", "Molly",
+		    "Alison", "Miranda", "Stephanie", "Leona", "Katrina", "Mila", "Teresa", "Gabriela", "Ashley", "Nicole",
+		    "Valentina", "Rose", "Juliana", "Alice", "Kathie", "Gloria", "Luna", "Phoebe", "Angelique", "Graciela",
+		    "Gemma", "Katelynn", "Danna", "Luisa", "Julie", "Olive", "Carolina", "Harmony", "Hanna", "Anabelle",
+		    "Francesca", "Whitney", "Skyla", "Nathalie", "Sophie", "Hannah", "Silvia", "Sophia", "Della", "Myra",
+		    "Blanca", "Bethany", "Robyn", "Traci", "Desiree", "Laverne", "Patricia", "Alberta", "Lynda", "Cara",
+		    "Brandi", "Janessa", "Claudia", "Rosa", "Sandra", "Eunice", "Kayla", "Kathryn", "Rosie", "Monique",
+		    "Maggie", "Hope", "Alexia", "Lucille", "Odessa", "Amanda", "Kimberly", "Blanche", "Tyra", "Helena",
+		    "Kayleigh", "Lucia", "Janine", "Maribel", "Camille", "Alisa", "Vivian", "Lesley", "Rachelle", "Kianna"
+        );
+	
+	List<String> lastNames = List.of(
+			"Williams", "Harris", "Thomas", "Robinson", "Walker", "Scott", "Nelson", "Mitchell", "Morgan", "Cooper",
+		    "Howard", "Davis", "Miller", "Martin", "Smith", "Anderson", "White", "Perry", "Clark", "Richards",
+		    "Wheeler", "Warburton", "Stanley", "Holland", "Terry", "Shelton", "Miles", "Lucas", "Fletcher", "Parks",
+		    "Norris", "Guzman", "Daniel", "Newton", "Potter", "Francis", "Erickson", "Norman", "Moody", "Lindsey", "Gross",
+		    "Sherman", "Simon", "Jones", "Brown", "Garcia", "Rodriguez", "Lee", "Young", "Hall", "Allen", "Lopez",
+		    "Green", "Gonzalez", "Baker", "Adams", "Perez", "Campbell", "Shaw", "Gordon", "Burns", "Warren", "Long",
+		    "Mcdonald", "Gibson", "Ellis", "Fisher", "Reynolds", "Jordan", "Hamilton", "Ford", "Graham", "Griffin",
+		    "Russell", "Foster", "Butler", "Simmons", "Flores", "Bennett", "Sanders", "Hughes", "Bryant", "Patterson",
+		    "Matthews", "Jenkins", "Watkins", "Ward", "Murphy", "Bailey", "Bell", "Cox", "Martinez", "Evans", "Rivera",
+		    "Peterson", "Gomez", "Murray", "Tucker", "Hicks", "Crawford", "Mason", "Rice", "Black", "Knight", "Arnold",
+		    "Wagner", "Mosby", "Ramirez", "Coleman", "Powell", "Singh", "Patel", "Wood", "Wright", "Stephens", "Eriksen",
+		    "Cook", "Roberts", "Holmes", "Kennedy", "Saunders", "Fisher", "Hunter", "Reid", "Stewart", "Carter", "Phillips",
+		    "Spencer", "Howell", "Alvarez", "Little", "Jacobs", "Foreman", "Knowles", "Meadows", "Richmond", "Valentine",
+		    "Dudley", "Woodward", "Weasley", "Livingston", "Sheppard", "Kimmel", "Noble", "Leach", "Gentry", "Lara",
+		    "Pace", "Trujillo", "Grant"
+        );
+	
+	List<String> cityNames = List.of(
+		    "Los Angeles", "Minsk", "Yangon", "Berlin", "Lima", "Alexandria", "Khartoum", "Kazan", "Kuala Lumpur", "Rome",
+		    "Bucharest", "Philadelphia", "Madrid", "Chennai", "Pune", "Kharkiv", "Birmingham", "Fukuoka", "Wuhan", "Guangzhou",
+		    "Foshan", "Surat", "Brussels", "Budapest", "Delhi", "Bangalore", "Guadalajara", "Miami", "Washington, D.C.",
+		    "Rostov-on-Don", "Nizhny Novgorod", "Cairo", "Hangzhou", "Riyadh", "Warsaw", "Kolkata", "Jinan", "Santiago",
+		    "Nanjing", "Singapore", "Bogotá", "São Paulo", "Kinshasa", "Dallas", "Dar es Salaam", "Nagoya", "Dalian", "Qingdao",
+		    "Chengdu", "Rio de Janeiro", "Seoul", "Karachi", "Toronto", "New York City", "Jakarta", "Suzhou", "Chongqing",
+		    "Atlanta", "Munich", "Beijing", "Perm", "Tianjin", "Harbin", "Dongguan", "Sofia", "Vienna", "Johannesburg",
+		    "Tokyo", "Shenyang", "Mumbai", "Shanghai", "Houston", "Istanbul", "Dhaka", "Kyiv", "Belgrade", "Baghdad",
+		    "Buenos Aires", "London", "Samara", "Volgograd", "Ho Chi Minh City", "Voronezh", "Saint Petersburg",
+		    "Belo Horizonte", "Chicago", "Lagos", "Ahmedabad", "Luanda", "Odessa", "Paris", "Prague", "Ufa", "Barcelona",
+		    "Hong Kong", "Lahore", "Osaka", "Shenzhen", "Milan"
+		);
+	
+	List<String> countryNames = List.of(
+		    "Palestine", "Grenada", "Mauritius", "Northern Mariana Islands", "Anguilla", "Germany", "Faroe Islands", "Virgin Islands", "Montserrat", "Singapore",
+		    "Afghanistan", "Argentina", "Benin", "Mongolia", "Mayotte", "Angola", "Andorra", "Burkina Faso", "Antigua and Barbuda", "Sierra Leone",
+		    "Réunion", "Finland", "Saint Helena, Ascension and Tristan da Cunha", "British Indian Ocean Territory", "Falkland Islands", "Cocos Islands", "Eritrea", "Uzbekistan", "Cameroon", "South Sudan",
+		    "Taiwan, Province of China", "Rwanda", "Burundi", "Korea", "India", "El Salvador", "Senegal", "Lao People's Democratic Republic", "Mexico", "Oman",
+		    "Cyprus", "Bulgaria", "France", "Cayman Islands", "Marshall Islands", "Albania", "Libya", "Turkmenistan", "Suriname", "Ireland",
+		    "Poland", "Greenland", "Nicaragua", "Bahrain", "Nauru", "Israel", "Ukraine", "Bangladesh", "Romania", "Timor-Leste",
+		    "Malawi", "Kuwait", "Australia", "Guernsey", "Japan", "Greece", "Virgin Islands", "Denmark", "Mali", "Armenia",
+		    "Zambia", "Paraguay", "Martinique", "San Marino", "Vietnam", "Dominican Republic", "Gabon", "Ivory Coast", "Yemen", "Seychelles",
+		    "Equatorial Guinea", "Iceland", "Guatemala", "Sweden", "Chad", "Belgium", "Bolivia", "Lesotho", "Congo", "Iran",
+		    "Switzerland", "Saint Barthélemy", "Nepal", "Cabo Verde", "Ethiopia", "Guadeloupe", "Jersey", "Bermuda", "Micronesia", "Hong Kong"
+		);
+	
+	
+	List<String> arrayNom = new ArrayList<String>();
 
     
 	@FXML
@@ -100,6 +167,12 @@ public class Controller {
 		return parser.tableArray.get(getCurrentTable()).size();
 	}
 	
+	public static String getRandomElement(List<String> list) {
+        Random random = new Random();
+        int index = random.nextInt(list.size());
+        return list.get(index);
+    }
+	
 	public void newTableArray() {
 
 				
@@ -116,7 +189,7 @@ public class Controller {
 					champLabels.add(new Label());
 					
 			        champLabels.get(i).setId("champ" + (i + 1));
-			        champLabels.get(i).setText(parser.tableArray.get(getCurrentTable()).get(i)); 
+			        champLabels.get(i).setText(parser.tableArray.get(getCurrentTable()).get(i));
 			        champLabels.get(i).setLayoutX(40.0);
 			        champLabels.get(i).setLayoutY(15.0 + i * 40.0); 
 			        champLabels.get(i).setPrefHeight(20.0);
@@ -127,7 +200,9 @@ public class Controller {
 			    	champChoiceBoxes.add(new ChoiceBox<String>());
 
 			        champChoiceBoxes.get(i).setId("champ" + (i + 1) + "choice");
-			        champChoiceBoxes.get(i).getItems().addAll("Please pick", "Primary Key", "Foreign Key", "First Name", "Last Name", "City", "Street");
+			        champChoiceBoxes.get(i).getItems().addAll("Please pick", "Primary Key", "Foreign Key",
+			        											"First Names", "Last Names", "City Names", "Street Names",
+			        											"Country Names");
 			        champChoiceBoxes.get(i).setValue("Please pick");
 			        champChoiceBoxes.get(i).setLayoutX(210.0);
 			        champChoiceBoxes.get(i).setLayoutY(13.0 + i * 40.0); 
@@ -143,9 +218,11 @@ public class Controller {
 				
 				//Make the height of the program be the same of the total height of all children
 				Platform.runLater(() -> {
-
+					primaryStage.hide();
+					primaryStage.show();
 				    double fullHeight = titlePanel.getHeight() + tablePanel.getHeight() + linePanel.getHeight() + submitPanel.getHeight() + 23 + 15;
 				    primaryStage.setHeight(fullHeight);
+				    
 				});
 	}
 	
@@ -178,11 +255,17 @@ public class Controller {
 	        
 	        fullArray.add(new ArrayList<>());
 	        
+	        
+	        
 	        for (int i = 0; i < lineNumbers; i++) {
 	
 		        fullArray.get(getCurrentTable()).add(new ArrayList<>());
-		        fullArray.get(getCurrentTable()).get(i).add(getTableName());
+		        //fullArray.get(getCurrentTable()).get(i).add(getTableName());
 	        
+		        /*for (int k = 0; k < getCurrentTableSize(); k++) {
+			        fullArray.get(0).get(k).add(parser.tableArray.get(getCurrentTable()).get(k));
+			    }*/
+		        
 		        for (int j = 1; j < champChoiceBoxes.size(); j++) {
 		        	
 		        	
@@ -194,16 +277,20 @@ public class Controller {
 			            	fullArray.get(getCurrentTable()).get(i).add(Integer.toString(i+1)); 
 			            }
 			            case "Foreign Key" -> {
+			            	
 			            	fullArray.get(getCurrentTable()).get(i).add(selectedValue);
 			            }
-			            case "First Name" -> {
-			            	fullArray.get(getCurrentTable()).get(i).add(selectedValue);
+			            case "First Names" -> {
+			            	fullArray.get(getCurrentTable()).get(i).add(getRandomElement(firstNames));
 			            }
-			            case "Last Name" -> {
-			            	fullArray.get(getCurrentTable()).get(i).add(selectedValue);
+			            case "Last Names" -> {
+			            	fullArray.get(getCurrentTable()).get(i).add(getRandomElement(lastNames));
 			            }
-			            case "City" -> {
-			            	fullArray.get(getCurrentTable()).get(i).add(selectedValue);
+			            case "City Names" -> {
+			            	fullArray.get(getCurrentTable()).get(i).add(getRandomElement(cityNames));
+			            }
+			            case "Country Names" -> {
+			            	fullArray.get(getCurrentTable()).get(i).add(getRandomElement(countryNames));
 			            }
 			            default -> {
 			            	fullArray.get(getCurrentTable()).get(i).add("didNotSelectAnythingImBlind");
